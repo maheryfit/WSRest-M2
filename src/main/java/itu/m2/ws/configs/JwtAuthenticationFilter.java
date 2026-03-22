e926a4fe51712dd4a5d04c4611020496fd24f7d8
Mahery <mahery_fitahiana@yahoo.com>
2026-03-22 12:28:54 +0300

    Configure password encoding and initialize JWT filter
    - Enable method-level security in `SecurityConfig` by adding the `@EnableMethodSecurity(prePostEnabled = true)` annotation.
    - Add a `PasswordEncoder` bean to `SecurityConfig` that utilizes `BCryptPasswordEncoder` for secure password hashing.
    - Create a new `JwtAuthenticationFilter` component extending `OncePerRequestFilter` with an initial skeleton `doFilterInternal` method to handle future JWT validation logic.

---
+++ src/main/java/itu/m2/ws/configs/JwtAuthenticationFilter.java
@@ -0,0 +1,19 @@
+package itu.m2.ws.configs;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
